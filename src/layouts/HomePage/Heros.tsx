1d5eccf31e6984f64158f42f30db9f3f3b315ac1
tosunismail <ismltsn881@gmail.com>
2024-04-28 22:44:01 +0300

    Added LibraryService and Footer component

--- src/layouts/HomePage/Heros.tsx
+++ src/layouts/HomePage/Heros.tsx
@@ -3,19 +3,18 @@ export const Heros = () => {
         <div>
             <div className='d-none d-lg-block'>
                 <div className='row g-0 mt-5'>
-                    <div className='col-sm-6col-md-6'>
-                        <div className='col-image-left'>
-                        </div>
-                        <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
-                            <div className='ml-2'>
-                                <h1>What have you been reading?</h1>
-                                <p className='lead'>
-                                    The library team would love to know what you have been reading.
-                                    Whetever it is to learn a nre skill or grow within one,
-                                    we will be able to provide the topcontent for you!
-                                </p>
-                                <a className='btn main-color btn-lg text-white' href='#'>Sign up</a>
-                            </div>
+                    <div className='col-sm-6 col-md-6'>
+                        <div className='col-image-left'></div>
+                    </div>
+                    <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
+                        <div className='ml-2'>
+                            <h1>What have you been reading?</h1>
+                            <p className='lead'>
+                                The library team would love to know what you have been reading.
+                                Whetever it is to learn a nre skill or grow within one,
+                                we will be able to provide the topcontent for you!
+                            </p>
+                            <a className='btn main-color btn-lg text-white' href='#'>Sign up</a>
                         </div>
                     </div>
                 </div>
@@ -38,6 +37,7 @@ export const Heros = () => {
                     </div>
                 </div>
             </div>
+
             {/* Mobile Heros */}
             <div className='d-lg-none'>
                 <div className='container'>
